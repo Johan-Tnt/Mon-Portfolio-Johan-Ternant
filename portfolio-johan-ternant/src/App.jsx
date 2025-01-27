534a2b98e311e3a06b698dd5fa768b1cce3ded8d
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2025-01-27 15:04:49 +0100

    Removal of unnecessary resources.

--- portfolio-johan-ternant/src/App.jsx
+++ portfolio-johan-ternant/src/App.jsx
@@ -1,8 +1,3 @@
-/*import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";*/
-//import "./App.css";
-
 //Début code Johan
 //Contient la/les routes des pages "home", "sign in" et "user" (react router)
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -26,35 +21,3 @@ const App = () => {
 
 export default App;
 //Fin code Johan
-
-/*function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1> Johan Ternant Portfolio </h1>
-      <h2> A propos </h2>
-      <h2> Projets </h2>
-      <h2> Contact </h2> npm install react-router-dom@6 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}*/
