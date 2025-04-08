c44cb7d6dceacdd8e2ef767c6a73946040673872
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2025-04-08 17:57:53 +0200

    Bug fixes.

--- portfolio-johan-ternant/vite.config.js
+++ portfolio-johan-ternant/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 //https://vite.dev/config/
 export default defineConfig({
-  //Définit la base en fonction de l'environnement : En production, la base est "/Mon-Portfolio-Johan-Ternant/" et en développement, la base est "/"
+  //Définit la base en fonction de l'environnement : En production, la base est "/Mon-Portfolio-Johan-Ternant/portfolio-johan-ternant/" et en développement, la base est "/"
   base:
     import.meta.env.MODE === "production"
       ? "/Mon-Portfolio-Johan-Ternant/portfolio-johan-ternant/"
