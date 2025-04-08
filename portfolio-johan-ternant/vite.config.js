36c6f567444edf6801deddc2982b1a37bb3c5777
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2025-04-08 14:50:33 +0200

    Added GitHub Pages workflow + CNAME + Added Portfolio favicon + Bug fixes.

--- portfolio-johan-ternant/vite.config.js
+++ portfolio-johan-ternant/vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 //https://vite.dev/config/
 export default defineConfig({
+  //Définit la base en fonction de l'environnement : En production, la base est "/Mon-Portfolio-Johan-Ternant/" et en développement, la base est "/"
+  base:
+    import.meta.env.MODE === "production"
+      ? "/Mon-Portfolio-Johan-Ternant/"
+      : "/",
   plugins: [react()],
   build: {
     minify: "esbuild", //Minification rapide
