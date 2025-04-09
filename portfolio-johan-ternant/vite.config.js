afc00d36b52c2d6d3834ea903b64374b3170885a
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2025-04-09 09:31:04 +0200

    Bug fixes vite.config.js.

--- portfolio-johan-ternant/vite.config.js
+++ portfolio-johan-ternant/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//https://vite.dev/config/
-export default defineConfig({
-  //Définit la base en fonction de l'environnement : En production, la base est "/Mon-Portfolio-Johan-Ternant/portfolio-johan-ternant/" et en développement, la base est "/"
-  base:
-    import.meta.env.MODE === "production"
-      ? "/Mon-Portfolio-Johan-Ternant/portfolio-johan-ternant/"
-      : "/",
-  plugins: [react()],
-  build: {
-    minify: "esbuild", //Minification rapide
-    rollupOptions: {
-      treeshake: true, //Active explicitement le tree shaking
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/Mon-Portfolio-Johan-Ternant/" : "/",
+    plugins: [react()],
+    build: {
+      minify: "esbuild", // Minification rapide
+      rollupOptions: {
+        treeshake: true, // Active explicitement le tree shaking
+      },
     },
-  },
+  };
 });
