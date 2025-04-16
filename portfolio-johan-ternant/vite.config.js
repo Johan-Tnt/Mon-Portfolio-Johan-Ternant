fc0504c8826ce7b3d63b2e54ef3440d17b7213a2
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2025-04-16 10:30:12 +0200

    Bug Fixes Update vite.config.js

--- portfolio-johan-ternant/vite.config.js
+++ portfolio-johan-ternant/vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 //https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  //export default defineConfig(() => {
+export default defineConfig(() => {
   return {
-    base: mode === "production" ? "/portfolio-johan-ternant/" : "/",
-    //base: "/", //important pour un domaine personnalisé
+    base: "/", //important pour un domaine personnalisé
     plugins: [react()],
     build: {
       minify: "esbuild", //Minification rapide
