e61f634b86f0fcee86a9fb98a277e96563a55aa3
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2025-04-22 10:30:42 +0200

    Added vite-plugin-image-optimizer.

--- portfolio-johan-ternant/vite.config.js
+++ portfolio-johan-ternant/vite.config.js
@@ -1,15 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 //https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
     base: "/", //important pour un domaine personnalisé
-    plugins: [react()],
+    plugins: [
+      react(),
+      ViteImageOptimizer({
+        includePublic: true,
+        logStats: true,
+        cache: true,
+        cacheLocation: "./.image-cache", //Dossier de cache
+        png: { quality: 80 },
+        jpeg: { quality: 80 },
+        webp: { lossless: true },
+        avif: { lossless: true },
+        svg: {
+          multipass: true,
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
+            },
+            "sortAttrs",
+          ],
+        },
+      }),
+    ],
     build: {
-      minify: "esbuild", //Minification rapide
+      minify: "esbuild",
       rollupOptions: {
-        treeshake: true, //Active explicitement le tree shaking
+        treeshake: true,
       },
     },
   };
